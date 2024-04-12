80fa48bf0a6e0780f49301c399323bcf0e8a6921
cagelmi <143134364+cagelmi@users.noreply.github.com>
2024-04-12 15:25:23 -0400

    Create problema_4d.m

---
+++ Prob4/problema_4d.m
@@ -0,0 +1,53 @@
+function problema_4d
+% Para ser utilizado con el texto H. Jorquera y C. Gelmi "Métodos Numéricos
+% Aplicados a Ingeniería: Casos de estudio en Ingeniería de Procesos usando
+% MATLAB", Ediciones UC, 2014.
+%
+% Última revisión: 12/04/2024.
+
+% Integración del sistema diferencial
+options = odeset('RelTol',1e-10,'AbsTol',1e-10);
+[t,x] = ode15s(@est,[0 500],[2 5 1e-8],options);
+
+% Grafica
+subplot(2,1,1)
+plot(t,x(:,1),'k','LineWidth',2)
+hold on
+plot(t,x(:,2),'k-.','LineWidth',2)
+hold off
+ylabel('Altura (m)')
+legend('h_1','h_2','Location','Best')
+subplot(2,1,2)
+plot(t,x(:,3),'k','LineWidth',2)
+xlabel('Tiempo (s)')
+ylabel('Velocidad (m/s)')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dx = est(t,x)
+
+% Parámetros y constantes
+cv = 0.1; D1 = 2.5; D2 = 3.0; Dt = 0.3;
+F0 = 0; g = 9.8;  h1max = 4.0;
+L = 100; mu = 0.001; ro = 1000; F2 = 0;
+
+% Relaciones constitutivas
+A1 = pi*D1^2/4; A2 = pi*D2^2/4;
+At = pi*Dt^2/4;
+Re = Dt*ro*abs(x(3))/mu;
+
+% Ecuaciones diferenciales
+dx = zeros(3,1);
+% Condición mínima para la velocidad en la cañería. % Valores menores no
+% son exitosos dado el paso de integración.
+% if x(1) >= 4 & abs(x(3)) > 0.05    
+%     dx(1) = 0;
+% else
+%     dx(1) = (F0-x(3)*At)/A1;
+% end
+if x(1) >= 4 & x(3) < 0
+    dx(1) = 0;
+else
+    dx(1) = (F0 - x(3)*At)/A1;
+end
+dx(2) = (x(3)*At-F2)/A2;
+dx(3) = g/L*(x(1)-x(2));
